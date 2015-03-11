d4828886b743011b20ea0265f394ff86e9d5643b
pi <pi@synyx.de>
2015-03-11 16:07:54 +0100

    Provide license

--- PrintServer/Properties/AssemblyInfo.cs
+++ PrintServer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("synyx GmbH & Co. KG")]
 [assembly: AssemblyProduct("LabelPrintServer")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
